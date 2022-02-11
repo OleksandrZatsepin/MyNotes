955c154985840e97c1abc9c62018cc3f08a257bf
Oleksandr Zatsepin <alexander.zatsepin@gmail.com>
2022-02-11 09:49:48 +0200

    Add tests.

---
+++ app/src/androidTest/java/dev/surehand/mynotes/feature_note/presentation/notes/NotesScreenTest.kt
@@ -0,0 +1,62 @@
+package dev.surehand.mynotes.feature_note.presentation.notes
+
+import android.content.Context
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.test.core.app.ApplicationProvider
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import dev.surehand.mynotes.core.util.TestTags
+import dev.surehand.mynotes.di.AppModule
+import dev.surehand.mynotes.feature_note.presentation.MainActivity
+import dev.surehand.mynotes.feature_note.presentation.util.Screen
+import dev.surehand.mynotes.ui.theme.MyNotesTheme
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+@UninstallModules(AppModule::class)
+class NotesScreenTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @ExperimentalAnimationApi
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+        composeRule.setContent {
+            val navController = rememberNavController()
+            MyNotesTheme {
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.NotesScreen.route
+                ) {
+                    composable(route = Screen.NotesScreen.route) {
+                        NotesScreen(navController = navController)
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun clickToggleOrderSection_isVisible() {
+        composeRule.onNodeWithTag(TestTags.ORDER_SECTION).assertDoesNotExist()
+        composeRule.onNodeWithContentDescription("Sort").performClick()
+        composeRule.onNodeWithTag(TestTags.ORDER_SECTION).assertIsDisplayed()
+    }
+}
